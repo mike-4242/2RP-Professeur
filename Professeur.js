e1b1667f85def6addb4787a8d7d2708b8a947bcd
Mikael Beaudet <mike-4242@hotmail.com>
2021-02-17 11:39:18 -0500

    ajout code test

--- Professeur.js
+++ Professeur.js
@@ -13,4 +13,35 @@ class EmployeUniversite {
     nomComplet() {
         return this.prenom + ", " + this.nom;
     }
+    
+    class EmployeUniversiteProfesseur extends EmployeUniversite {
+        constructor(nom, prenom, numeroEmploye, anneesExperience, salaire, coursEnseigne) {
+            super(nom, prenom, numeroEmploye, anneesExperience);
+            this.salaire = salaire;
+            this.coursEnseigne = coursEnseigne;
+        }
+    
+        ajouterUnCours(nouveauCours) {
+            this.coursEnseigne[this.coursEnseigne.length] = nouveauCours;
+        }
+    }
 }
+
+employe = new EmployeUniversite('Pierre', 'Martel', 'E0004', 5);
+employe.augmenterAnciennete();
+console.log(employe.nomComplet());
+console.log(employe.anneesExperience);
+
+
+prof = new EmployeUniversiteProfesseur('Carmel', 'Simon', 'P0007', 4, 65000, []);
+prof.augmenterAnciennete();
+console.log(prof.nomComplet());
+console.log(prof.anneesExperience);
+console.log(prof.salaire);
+console.log(prof.coursEnseigne);
+console.log(prof.information());
+
+prof.ajouterUnCours('2RP');
+console.log(prof.coursEnseigne);
+prof.ajouterUnCours('CG2');
+console.log(prof.coursEnseigne);
